b62b00321008e1562ce525e6256d24ebca66f22f
Arthur <arthur@cloudawesome.uk>
2020-05-11 17:09:26 +0100

    Extract user credentials to app.config; Support multiple users/personae; Rebuild in .Net to support non-Core requirements

--- CloudAwesome.PortalTestFramework/Models/UserCredentials.cs
+++ CloudAwesome.PortalTestFramework/Models/UserCredentials.cs
@@ -1,14 +1,39 @@
-﻿namespace CloudAwesome.PortalTestFramework.Models
+﻿using System;
+using System.Configuration;
+using System.Collections.Generic;
+
+namespace CloudAwesome.PortalTestFramework.Models
 {
     public enum AuthenticationType
     {
         ActiveDirectory, Local, Facebook, Google
     }
 
-    public class UserCredentials
+    public class UserCredentials: ConfigurationElement
     {
-        public string UserName { get; set; }
-        public string UserPassword { get; set; }
-        public AuthenticationType AuthenticationType { get; set; }
+        [ConfigurationProperty("Id", IsRequired = true, IsKey = true)]
+        public string PersonaId => (string) this["Id"];
+
+        [ConfigurationProperty("UserName", IsRequired = true)]
+        public string UserName => (string) this["UserName"];
+
+        [ConfigurationProperty("UserPassword", IsRequired = true)]
+        public string UserPassword => (string) this["UserPassword"];
+
+        [ConfigurationProperty("AuthenticationType", IsRequired = true)]
+        public AuthenticationType AuthenticationType => (AuthenticationType) this["AuthenticationType"];
+    }
+
+    public class UserCredentialCollection: ConfigurationElementCollection
+    {
+        protected override ConfigurationElement CreateNewElement()
+        {
+            return new UserCredentials();
+        }
+
+        protected override object GetElementKey(ConfigurationElement element)
+        {
+            return ((UserCredentials) element).PersonaId;
+        }
     }
 }
--- DynamicsPortalTestFramework/SolutionCreationTests.cs
+++ DynamicsPortalTestFramework/SolutionCreationTests.cs
@@ -1,4 +1,7 @@
-﻿using NUnit.Framework;
+﻿using System.Configuration;
+using System.Linq;
+
+using NUnit.Framework;
 
 using CloudAwesome.PortalTestFramework;
 using CloudAwesome.PortalTestFramework.Models;
@@ -8,32 +11,28 @@ namespace CloudAwesome.PortalTests
     [TestFixture]
     public class SolutionCreationTests
     {
+        [Test]
+        public void TestConfigSettings()
+        {
+            var config = new PortalConfiguration("arthur");
 
+            Assert.AreEqual("arthur", config.UserCredentials.UserName);
+            Assert.AreEqual("https://awesome-sandbox.microsoftcrmportals.com", config.BaseUrl);
+            //Assert.AreEqual();
+        }
+        
         [Test]
         [Category("Initial Project Creation")]
         [Category("Firefox")]
         public void FirstTestDuringSetUp()
         {
-            //TODO - Extract config details out of the test class
-            //TODO - Support multiple signins for different personas (constructor to determine config which to use?)
+            #region actions to do 
             //TODO - Support for multiple browsers
             //TODO - Initialisation helper - set up and tear down of local user account
             // Each class should be as test small as possible and include as little set up as possible.
             // ^^ On average, 10-20 lines max. would be great :)
-            var config = new PortalConfiguration()
-            {
-                BaseUrl = "https://awesome-sandbox.microsoftcrmportals.com",
-                UserCredentials = new UserCredentials()
-                {
-                    UserName = "arthur",
-                    UserPassword = "zxasq1!",
-                    AuthenticationType = AuthenticationType.Local
-                },
-                BrowserSettings = new BrowserSettings()
-                {
-                    BrowserType = BrowserType.Firefox
-                }
-            };
+            #endregion actions to do
+            var config = new PortalConfiguration("arthur");
 
             // Arrange
             var portal = new Portal(config);
@@ -46,16 +45,16 @@ namespace CloudAwesome.PortalTests
             // Act
             var result = 
                 portal
-                    .SetValue("awe_forename", "Arthur")
+                    .SetValue("firstname", "Arthur") // TODO - Extract magic strings out into DataModel
+                    .SetValue("lastname", "Nicholson-Gumuła")
                     .Click("submit")
                     .Wait(1000)
-                    .GetValue("awe_name");
+                    .GetValue("name");
 
             // Assert
-            Assert.AreEqual("Arthur's Record", result, 
-                "Test result is happy ;)");
+            Assert.AreEqual("Arthur Nicholson-Gumuła", result, 
+                "Record name has been set correctly");
 
-            //TODO - Extract this out into a generic teardown method so doesn't need to be in every test class?
             portal.Quit();
 
         }
